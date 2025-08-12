f6a8aeaef3bd62483c8035726cb59a7322c07c61
danizoker <demoura.9998@gmail.com>
2025-08-12 11:10:18 -0300

    Add files via upload

---
+++ 002.py
@@ -0,0 +1,7 @@
+""" faça um programa que peça as 4 notas bimestrais e mostra a media!."""
+numero1=input("digite a primeira nota do bimestre:")
+numero2=input("digite a segunda nota do bimestre:")
+numero3=input("digite a terceira nota do bimestre:")
+numero4=input("digite a nota do quarto bimestre:")
+soma=int(numero1)+int(numero2)+int(numero3)+int(numero4)
+"faça um programa que calcule total dos bemetres semestrais"
