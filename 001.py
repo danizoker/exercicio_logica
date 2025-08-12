8b48d42c8d8f8f63a00531625f18f85f1d11c9aa
danizoker <demoura.9998@gmail.com>
2025-08-12 10:54:24 -0300

    Add files via upload

---
+++ 001.py
@@ -0,0 +1,20 @@
+"""
+faça um programa que peça um numero e então mostre a mensagem .
+o numero informado é [ número ] .
+ variável  = inpunt - entrada do numero .
+ mostrar na tela
+numero  = input("digite um numero : ")
+
+print( numero informado é :" , numero)
+
+"""
+numero1 = input("digite o primeiro número inteiro: ")
+numero2 = input("digite o segundo númeiro inteiro: ")
+soma = int(numero1) + int(numero2)
+
+""""
+faça um programa que peça dois numeros inteiros inprima a soma desses dois numero
+
+"""
+
+ 
